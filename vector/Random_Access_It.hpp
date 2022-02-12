204a80b522ece3ce3f3b452ff6c901712c3cff0f
Yacine Hadari <yhadari@e1r2p4.1337.ma>
2022-02-12 21:05:42 +0100

    start iterator

--- vector/Random_Access_It.hpp
+++ vector/Random_Access_It.hpp
@@ -8,15 +8,64 @@ class Random_Access_It : public Iterator<std::random_access_iterator_tag, T>{
 
     T* _ptr;
 
+    typedef typename Iterator<std::random_access_iterator_tag, T>::value_type value_type;
+    typedef typename Iterator<std::random_access_iterator_tag, T>::difference_type difference_type;
+    typedef typename Iterator<std::random_access_iterator_tag, T>::Pointer Pointer;
+    typedef typename Iterator<std::random_access_iterator_tag, T>::Reference Reference;
+    typedef typename Iterator<std::random_access_iterator_tag, T>::iterator_category iterator_category;
+
     public:
     Random_Access_It(T* p): _ptr(p){}
-    Random_Access_It(Random_Access_It const& copy){*this = copy;};
-    Random_Access_It&   operator=(Random_Access_It const& it){this->_ptr = it._ptr;};
+    Random_Access_It(Random_Access_It const& copy): _ptr(copy._ptr){}
+    Random_Access_It&   operator=(Random_Access_It const& it){
+        this->_ptr = it._ptr;
+    };
+    Random_Access_It&   operator++(){
+        this->_ptr++;
+        return *this;
+    };
+    Random_Access_It&   operator++(int){
+        Random_Access_It old_ptr(*this);
+        this->_ptr++;
+        return old_ptr;
+    };
+    Random_Access_It&   operator--(){
+        this->_ptr--;
+        return *this;
+    };
+    Random_Access_It&   operator--(int){
+        Random_Access_It old_ptr(*this);
+        this->_ptr--;
+        return old_ptr;
+    };
+    Random_Access_It&   operator-(Random_Access_It const& it){
+        this->_ptr -= it._ptr;
+        return *this;
+    };
+    Random_Access_It&   operator+(difference_type& value){
+        Random_Access_It new_ptr(*this+value);
+        return new_ptr;
+    };
+    Random_Access_It&   operator-(difference_type const& value){
+        Random_Access_It new_ptr(*this-value);
+        return new_ptr;
+    };
     bool                operator==(Random_Access_It const& it){
         return  this->_ptr == it._ptr ? true : false;
-    }  
-    ~Random_Access_It(void){};    
-
+    }
+    bool                operator!=(Random_Access_It const& it){
+        return  !operator==(it);
+    }
+    T&                  operator*(){
+        return  *this->_ptr;
+    }
+    T&                  operator->(){
+        return this->_ptr;
+    }
+    T&                  operator*(T const& t){
+        return (*this->_ptr = t);
+    }   
+    ~Random_Access_It(void){};
 };
 
 #endif
--- vector/test.cpp
+++ vector/test.cpp
@@ -1,27 +1,8 @@
 #include <iostream>     // std::cout
-#include <iterator>     // std::iterator, std::input_iterator_tag
-
-class MyIterator : public std::iterator<std::input_iterator_tag, int>
-{
-  int* p;
-public:
-  MyIterator(int* x) :p(x) {}
-  MyIterator(const MyIterator& mit) : p(mit.p) {}
-  MyIterator& operator++() {++p;return *this;}
-  MyIterator operator++(int) {MyIterator tmp(*this); operator++(); return tmp;}
-  bool operator==(const MyIterator& rhs) const {return p==rhs.p;}
-  bool operator!=(const MyIterator& rhs) const {return p!=rhs.p;}
-  int& operator*() {return *p;}
-};
+#include <iterator>
+#include <vector>
+#include <stdio.h>
 
 int main () {
-  int numbers[]={10,20,30,40,50};
-  MyIterator from(numbers);
-  MyIterator until(numbers+5);
-  while (from!=until){
-    std::cout << *from << ' ' << std::endl;
-    from++;
-  }
 
-  return 0;
 }
