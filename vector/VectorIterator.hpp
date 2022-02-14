b839aea85956200995f1ed859e9e493436abb343
Yacine Hadari <yhadari@e1r2p2.1337.ma>
2022-02-14 15:56:20 +0100

    finish vectorIterator

--- vector/VectorIterator.hpp
+++ vector/VectorIterator.hpp
@@ -18,58 +18,79 @@ class VectorIterator : public Iterator<std::random_access_iterator_tag, T>{
 
     VectorIterator(T* p): _ptr(p){}
     VectorIterator(VectorIterator const& copy): _ptr(copy._ptr){}
-    VectorIterator&   operator=(VectorIterator const& it){
+    VectorIterator&     operator=(VectorIterator const& it){
         this->_ptr = it._ptr;
         return *this;
     };
-    VectorIterator&   operator++(){
+    VectorIterator&     operator++(){
         this->_ptr++;
         return *this;
     };
-    VectorIterator&   operator++(int){
+    VectorIterator      operator++(int){
         VectorIterator old_it(*this);
         this->_ptr++;
         return old_it;
     };
-    VectorIterator&   operator--(){
+    VectorIterator&     operator--(){
         this->_ptr--;
         return *this;
     };
-    VectorIterator&   operator--(int){
+    VectorIterator      operator--(int){
         VectorIterator old_it(*this);
         this->_ptr--;
         return old_it;
     };
-    difference_type   operator-(VectorIterator& it){
+    difference_type     operator-(VectorIterator& it){
         difference_type Distance = this->_ptr - it._ptr;
         return Distance;
     };
-    VectorIterator&   operator+(difference_type value){
+    VectorIterator      operator+(difference_type value){
         return VectorIterator(this->_ptr+value);
     };
-    VectorIterator&   operator-(difference_type value){
+    VectorIterator      operator-(difference_type value){
         return VectorIterator(this->_ptr-value);
     };
+    VectorIterator&     operator+=(difference_type value){
+        return *this = VectorIterator(this->_ptr+value);
+    };
+    VectorIterator&     operator-=(difference_type value){
+        return *this = VectorIterator(this->_ptr-value);
+    };
     bool                operator==(VectorIterator const& it){
-        return  this->_ptr == it._ptr ? true : false;
+        return  this->_ptr == it._ptr;
     }
     bool                operator!=(VectorIterator const& it){
-        return  !operator==(it);
+        return  this->_ptr != it._ptr;
+    }
+    bool                operator<(VectorIterator const& it){
+        return this->_ptr < it._ptr;
+    }
+    bool                operator>(VectorIterator const& it){
+        return this->_ptr > it._ptr;
+    }
+    bool                operator<=(VectorIterator const& it){
+        return this->_ptr <= it._ptr;
+    }
+    bool                operator>=(VectorIterator const& it){
+        return this->_ptr >= it._ptr;
     }
     T&                  operator*(){
         return  *this->_ptr;
     }
+    T&                  operator*(T const& t){
+        return (*this->_ptr = t);
+    }
     T&                  operator->(){
         return this->_ptr;
     }
-    T&                  operator*(T const& t){
-        return (*this->_ptr = t);
+    T&                  operator[](difference_type index){
+        return this->_ptr[index];
     }
     ~VectorIterator(void){};
 };
 
 template<typename T>
-VectorIterator<T>&   operator+(typename VectorIterator<T>::difference_type value, VectorIterator<T> it){
-    return VectorIterator<T>(it._ptr+value);
+VectorIterator<T>       operator+(typename VectorIterator<T>::difference_type value, VectorIterator<T> it){
+    return it+value;
 };
 #endif
--- vector/test.cpp
+++ vector/test.cpp
@@ -5,29 +5,26 @@
 
 int main () {
     {
-        int *p1 = new int[3];
+        int *p1 = new int[4];
         p1[0] = 10;
         p1[1] = 20;
         p1[2] = 30;
 
-        int *p2 = new int[3];
+        int *p2 = new int[4];
         p2[0] = 11;
         p2[1] = 21;
         p2[2] = 31;
-        VectorIterator<int> i1(p1);
-        VectorIterator<int> i2(p2);
-        ptrdiff_t d;
-        d = i2 - i1;
-        std::cout << d << std::endl;
+        VectorIterator<int> i1 = p1;
+        VectorIterator<int> i2 = p2;
+        std::cout << i1[0] << std::endl;
     }
 
     {
-        std::vector<int> p1(3, 10);
-        std::vector<int> p2(3, 20);
+        std::vector<int> p1(4, 10);
+        std::vector<int> p2(4, 20);
         std::vector<int>::iterator it1 = p1.begin();
         std::vector<int>::iterator it2 = p2.begin();
-        ptrdiff_t d;
-        d = it2 - it1;
-        std::cout << d << std::endl;
+        bool i = it1 >= it2;
+        std::cout << i << std::endl;
     }
 }
