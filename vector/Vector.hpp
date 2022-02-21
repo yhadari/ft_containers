fd426ac30720f00f1af3d7ba6d4ba95ae2321e53
Yacine Hadari <yhadari@e1r5p1.1337.ma>
2022-02-21 19:38:12 +0100

    add enable_if

--- vector/Vector.hpp
+++ vector/Vector.hpp
@@ -3,24 +3,43 @@
 
 #include "VectorIterator.hpp"
 #include "Reverse_iterator.hpp"
+#include "enable_if.hpp"
 
-template < class T, class Alloc = std::allocator<T> >
-class Vector{
+namespace ft{
 
-    public:
+    template < class T, class Alloc = std::allocator<T> >
+    class vector{
+    
+        T *_array;
+    
+        public:
+    
+        typedef             T                                               value_type;
+        typedef             Alloc                                           allocator_type;
+        typedef typename    allocator_type::reference                       reference;
+        typedef typename    allocator_type::const_reference                 const_reference;
+        typedef typename    allocator_type::pointer                         pointer;
+        typedef typename    allocator_type::const_pointer                   const_pointer;
+        typedef             ft::VectorIterator<T>                           iterator;
+        typedef             const   iterator                                const_iterator;
+        typedef             ft::reverse_iterator<iterator>                  reverse_iterator;
+        typedef             ft::reverse_iterator<const_iterator>            const_reverse_iterator;
+        typedef typename    ft::iterator_traits<iterator>::difference_type  difference_type;
+        typedef             size_t                                          size_type;
 
-    typedef             T                                               value_type;
-    typedef             Alloc                                           allocator_type;
-    typedef typename    allocator_type::reference                       reference;
-    typedef typename    allocator_type::const_reference                 const_reference;
-    typedef typename    allocator_type::pointer                         pointer;
-    typedef typename    allocator_type::const_pointer                   const_pointer;
-    typedef             ft::VectorIterator<T>                           iterator;
-    typedef             const   iterator                                const_iterator;
-    typedef             ft::reverse_iterator<iterator>                  reverse_iterator;
-    typedef             ft::reverse_iterator<const_iterator>            const_reverse_iterator;
-    typedef typename    ft::iterator_traits<iterator>::difference_type  difference_type;
-    typedef             size_t                                          size_type;
-};
-
+        explicit vector(const allocator_type& alloc = allocator_type()){
+            allocator_type allocat(alloc);
+            this->_array = allocat.allocate(0);
+        }
+        explicit vector (size_type n, const value_type& val = value_type(),
+                     const allocator_type& alloc = allocator_type()){
+                         std::cout << "1" << std::endl;
+                     }
+        template <class InputIterator>
+             vector (InputIterator first, InputIterator last,
+                     const allocator_type& alloc = allocator_type(), typename enable_if<!std::is_integral<InputIterator>::value, InputIterator>::type = InputIterator()){
+                        std::cout << "2" << std::endl;
+                     }
+    };
+}
 #endif
--- vector/test.cpp
+++ vector/test.cpp
@@ -1,17 +1,11 @@
-// reverse_iterator::base example
-#include <iostream>     // std::cout
-#include <iterator>     // std::reverse_iterator
-#include <vector>       // std::vector
-#include "VectorIterator.hpp"
+#include "Vector.hpp"
+#include "vector"
 
-int main () {
+int main(){
 
-  std::vector<int> v(10,10);
-  int *p = new int[4];
-  p[0] = 10;
-  p[0] = 11;
-  p[0] = 12;
-  p[0] = 13;
-  VectorIterator<int>::Iterator i = p;
-  return 0;
+    std::vector<int> p1(10, 10);
+    std::vector<int>::iterator itb = p1.begin();
+    std::vector<int>::iterator ite = p1.end();
+    ft::vector<int>p2(10, 10);
+    ft::vector<int>p3(itb, ite);
 }
