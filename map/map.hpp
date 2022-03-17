386b3bcb5214a22f052c9d581e7be359d58cc335
Yacine Hadari <yhadari@e1r2p7.1337.ma>
2022-03-17 18:43:51 +0100

    add pair && make_pair

---
+++ map/map.hpp
@@ -0,0 +1,12 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+namespace ft{
+
+    template < class Key, class T, class Compare = less<Key>, class Alloc = std::allocator<pair<const Key,T> > >
+    class map{
+
+    };
+};
+
+#endif
