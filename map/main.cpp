5c9f990ddcb16a7c8992bcf49edf2325f87ffdea
Yacine Hadari <yhadari@e1r2p7.1337.ma>
2022-03-17 16:08:14 +0100

    finish avl tree

--- map/main.cpp
+++ map/main.cpp
@@ -1,200 +1,190 @@
-#include "iostream"
+// AVL tree implementation in C++
 
-struct node
-{
-    int key;
-    struct node *left;
-    struct node *right;
-};
+#include <iostream>
+using namespace std;
 
-struct node* newNode(int item)
-{
-    struct node* temp = (struct node*)malloc(sizeof(struct node));
-    temp->key = item;
-    temp->right = NULL;
-    temp->left = NULL;
-    return temp;
-}
+typedef struct  s_node {
+  int       key;
+  s_node    *left;
+  s_node    *right;
+  int       height;
+}               t_node;
 
-struct node* insert(struct node* node, int key)
-{
-    if (node == NULL)
-        return newNode(key);
-    if (key < node->key)
-        node->left = insert(node->left, key);
-    else if (key > node->key)
-        node->right = insert(node->right, key);
-    return node;
-}
-
-void inorder(struct node* root)
-{
-    if (root != NULL)
-    {
-        inorder(root->left);
-        std::cout << root->key << " ";
-        inorder(root->right);
-    }
-}
-
-void preOrder(struct node* root)
-{
-    if (root != NULL) {
-        std::cout << " " << root->key;
-        preOrder(root->left);
-        preOrder(root->right);
-    }
-}
-
-void postOrder(struct node* root)
-{
-    if (root != NULL)
-    {
-        postOrder(root->left);
-        postOrder(root->right);
-        std::cout << " " << root->key;
-    }
-}
-
-int height(struct node* node)
-{
-    if (node == NULL)
-        return 0;
-    else {
-        int lDepth = height(node->left);
-        int rDepth = height(node->right);
-        if (lDepth > rDepth)
-            return (lDepth + 1);
-        else
-            return (rDepth + 1);
-    }
-}
-
-void printGivenLevel(struct node* root, int level)
-{
-    if (root == NULL)
-        return;
-    if (level == 1)
-        std::cout <<"  "<< root->key;
-    else if (level > 1) {
-        printGivenLevel(root->left, level - 1);
-        printGivenLevel(root->right, level - 1);
-    }
-}
-
-void printLevelOrder(struct node* root)
-{
-    int h = height(root);
-    for (int i = 1; i <= h; i++) {
-        printGivenLevel(root, i);
-        std::cout <<"\n";
-    }
-}
-
-struct node* minValueNode(struct node* node)
-{
-    while (node->left)
-        node = node->left;
-    return node;
-}
-
-void leftViewUtil(struct node* root, int level, int* max_level)
-{
-    if (root == NULL)
-        return;
-    if (*max_level < level) {
-        std::cout <<"  "<< root->key;
-        *max_level = level;
-    }
-    leftViewUtil(root->left, level + 1, max_level);
-    leftViewUtil(root->right, level + 1, max_level);
-}
- 
-void leftView(struct node* root)
-{
-    int max_level = 0;
-    leftViewUtil(root, 1, &max_level);
-}
-
-void rightViewUtil(struct node* root, int level, int* max_level)
-{
-    if (root == NULL)
-        return;
-    if (*max_level < level) {
-        std::cout <<" "<< root->key;
-        *max_level = level;
-    }
-    rightViewUtil(root->right, level + 1, max_level);
-    rightViewUtil(root->left, level + 1, max_level);
-}
-
-void rightView(struct node* root)
-{
-    int max_level = 0;
-    rightViewUtil(root, 1, &max_level);
-}
-
-struct node* deleteNode(struct node* root, int key)
-{
-    if (root == NULL)
-        return root;
-    if (key < root->key)
-        root->left = deleteNode(root->left, key);
-    else if (key > root->key)
-        root->right = deleteNode(root->right, key);
-    else {
-        if (root->left == NULL) {
-            struct node* temp = root->right;
-            free(root);
-            return temp;
-        }
-        else if (root->right == NULL) {
-            struct node* temp = root->left;
-            free(root);
-            return temp;
-        }
-        struct node* temp = minValueNode(root->right);
-        root->key = temp->key;
-        root->right = deleteNode(root->right, temp->key);
-    }
-    return root;
-}
-
-int nodeCount(struct node* node)
-{
-    if (node == NULL)
-        return 0;
-    else
-        return (nodeCount(node->left) + nodeCount(node->right) + 1);
-}
-
-int main()
-{
-    /* Let us create following BST
-              50
-           /     \
-          30      70
-         /  \    /  \
-       20   40  60   80
-   */
-    struct node* root = NULL;
- 
-    // Creating the BST
-    root = insert(root, 50);
-    insert(root, 30);
-    insert(root, 20);
-    insert(root, 40);
-    insert(root, 70);
-    insert(root, 60);
-    insert(root, 80);
- 
-    // Function Call
-    root = deleteNode(root, 50);
-    leftView(root);
-    std::cout << std::endl;
-    rightView(root);
-    std::cout << std::endl;
-    inorder(root);
-    std::cout << std::endl << " "<< nodeCount(root);
+// Calculate height
+int height(t_node *N) {
+  if (N == NULL)
     return 0;
+  return N->height;
+}
+
+int max(int a, int b) {
+  return (a > b) ? a : b;
+}
+
+// New node creation
+t_node *newt_node(int key) {
+  t_node *node = new t_node();
+  node->key = key;
+  node->left = NULL;
+  node->right = NULL;
+  node->height = 1;
+  return (node);
+}
+
+// Rotate right
+t_node *rightRotate(t_node *y) {
+  t_node *x = y->left;
+  t_node *T2 = x->right;
+  x->right = y;
+  y->left = T2;
+  y->height = max(height(y->left), height(y->right)) + 1;
+  x->height = max(height(x->left), height(x->right)) + 1;
+  return x;
+}
+
+// Rotate left
+t_node *leftRotate(t_node *x) {
+  t_node *y = x->right;
+  t_node *T2 = y->left;
+  y->left = x;
+  x->right = T2;
+  x->height = max(height(x->left), height(x->right)) + 1;
+  y->height = max(height(y->left), height(y->right)) + 1;
+  return y;
+}
+
+// Get the balance factor of each node
+int getBalanceFactor(t_node *N) {
+  if (N == NULL)
+    return 0;
+  return height(N->left) - height(N->right);
+}
+
+// Insert a node
+t_node *insertt_node(t_node *node, int key) {
+  // Find the correct postion and insert the node
+  if (node == NULL)
+    return (newt_node(key));
+  if (key < node->key)
+    node->left = insertt_node(node->left, key);
+  else if (key > node->key)
+    node->right = insertt_node(node->right, key);
+  else
+    return node;
+
+  // Update the balance factor of each node and
+  // balance the tree
+  node->height = 1 + max(height(node->left), height(node->right));
+  int balanceFactor = getBalanceFactor(node);
+  if (balanceFactor > 1) {
+    if (key < node->left->key) {
+      return rightRotate(node);
+    } else if (key > node->left->key) {
+      node->left = leftRotate(node->left);
+      return rightRotate(node);
+    }
+  }
+  if (balanceFactor < -1) {
+    if (key > node->right->key) {
+      return leftRotate(node);
+    } else if (key < node->right->key) {
+      node->right = rightRotate(node->right);
+      return leftRotate(node);
+    }
+  }
+  return node;
+}
+
+// t_node with minimum value
+t_node *nodeWithMimumValue(t_node *node) {
+  t_node *current = node;
+  while (current->left != NULL)
+    current = current->left;
+  return current;
+}
+
+// Delete a node
+t_node *deletet_node(t_node *root, int key) {
+  // Find the node and delete it
+  if (root == NULL)
+    return root;
+  if (key < root->key)
+    root->left = deletet_node(root->left, key);
+  else if (key > root->key)
+    root->right = deletet_node(root->right, key);
+  else {
+    if ((root->left == NULL) || (root->right == NULL)) {
+      t_node *temp = root->left ? root->left : root->right;
+      if (temp == NULL) {
+        temp = root;
+        root = NULL;
+      } else
+        *root = *temp;
+      free(temp);
+    } else {
+      t_node *temp = nodeWithMimumValue(root->right);
+      root->key = temp->key;
+      root->right = deletet_node(root->right, temp->key);
+    }
+  }
+
+  if (root == NULL)
+    return root;
+
+  // Update the balance factor of each node and
+  // balance the tree
+  root->height = 1 + max(height(root->left), height(root->right));
+  int balanceFactor = getBalanceFactor(root);
+  if (balanceFactor > 1) {
+    if (getBalanceFactor(root->left) >= 0) {
+      return rightRotate(root);
+    } else {
+      root->left = leftRotate(root->left);
+      return rightRotate(root);
+    }
+  }
+  if (balanceFactor < -1) {
+    if (getBalanceFactor(root->right) <= 0) {
+      return leftRotate(root);
+    } else {
+      root->right = rightRotate(root->right);
+      return leftRotate(root);
+    }
+  }
+  return root;
+}
+
+// Print the tree
+void printTree(t_node *root, string indent, bool last) {
+  if (root != nullptr) {
+    cout << indent;
+    if (last) {
+      cout << "R----";
+      indent += "   ";
+    } else {
+      cout << "L----";
+      indent += "|  ";
+    }
+    cout << root->key << endl;
+    printTree(root->left, indent, false);
+    printTree(root->right, indent, true);
+  }
+}
+
+int main() {
+  t_node *root = NULL;
+  root = insertt_node(root, 33);
+  root = insertt_node(root, 13);
+  root = insertt_node(root, 53);
+  root = insertt_node(root, 9);
+  root = insertt_node(root, 21);
+  root = insertt_node(root, 61);
+  root = insertt_node(root, 8);
+  root = insertt_node(root, 11);
+  std::cout<<"balance nb : "<<getBalanceFactor(root)<<std::endl;
+  printTree(root, "", true); 
+  root = insertt_node(root, 0);
+  cout << "After deleting " << endl;
+  std::cout<<"balance nb : "<<getBalanceFactor(root)<<std::endl;
+  printTree(root, "", true);
 }
